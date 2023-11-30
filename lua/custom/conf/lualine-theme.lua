5edcb9932a204b8ac2842bc14c1568cf7accbdbf
Vincent Gschwend <12810603+neonima@users.noreply.github.com>
2023-11-30 18:44:55 +0000

    fix: lualine to show project path

--- lua/custom/conf/lualine-theme.lua
+++ lua/custom/conf/lualine-theme.lua
@@ -37,7 +37,7 @@ require('lualine').setup {
     lualine_a = {
       { 'mode', separator = { left = '' }, right_padding = 2 },
     },
-    lualine_b = { 'filename', 'branch' },
+    lualine_b = { { 'filename', path = 1 }, { 'branch' } },
     lualine_c = { 'fileformat' },
     lualine_x = {},
     lualine_y = { 'filetype', 'progress' },
